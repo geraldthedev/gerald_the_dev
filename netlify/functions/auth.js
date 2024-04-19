fe33638672acc3a0ac199e93dee83385be789ac7
Gerald Bryant <25540747+geraldthedev@users.noreply.github.com>
2024-04-19 17:25:19 -0400

    Update time

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -20,7 +20,7 @@ import jwt from 'jsonwebtoken';
             const payload ={
               external_id: '8987738',
                email: "victortest@testmail.com",
-               iat: new Date(),
+               iat: new Date().getTime(),
                name: "Victor Test",
                scope: "user"
             
